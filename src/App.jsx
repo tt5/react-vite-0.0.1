3ec258186bd78bb21eabfd814bd6df8ecdd7b9bc
n <2585803+tt5@users.noreply.github.com>
2022-07-04 18:34:48 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import Mycomponent from './components/Mycomponent'
+
+function App() {
+  return (
+    <div className="App">
+      <h1>hello</h1>
+      <Mycomponent />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Mycomponent.jsx
@@ -0,0 +1,7 @@
+function Mycomponent() {
+  return (
+    <p>Text.</p>
+  )
+}
+
+export default Mycomponent
