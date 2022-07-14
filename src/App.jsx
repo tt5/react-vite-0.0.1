fde989f7032f5a1fcd9b73b9761676ebbc0f63a7
n <2585803+tt5@users.noreply.github.com>
2022-07-14 15:39:59 +0200

    children props example

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import './styles/App.css'
 import Mycomponent from './components/Mycomponent'
 import Nav from './components/Nav'
+import Border from './components/Border'
 
 function App() {
   return (
     <div className="App">
       <Nav title={"react-vite-tailwind"} />
       <Mycomponent />
+      <Border size="4">hello</Border>
     </div>
   )
 }
---
+++ src/components/Border.jsx
@@ -0,0 +1,8 @@
+export default function Border({ children, size = 2}) {
+  return (
+    <div className={`border-solid border-${size}`}>
+      {children}
+    </div>
+  )
+}
+
